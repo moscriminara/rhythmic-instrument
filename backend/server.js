ed722c3fbcf6ee6d53dca1ab177890625091bda5
kanozkuyo <kanozkuyo@gmail.com>
2026-07-24 21:50:25 +1000

    test02

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,7 @@ app.use(express.json())
 
 const db = mysql.createPool({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
