ae8c241367e87a28bc6aeac3f038222b90cef0e0
kanozkuyo <kanozkuyo@gmail.com>
2026-07-24 21:48:41 +1000

    test

--- backend/server.js
+++ backend/server.js
@@ -362,4 +362,22 @@ app.post('/upload-result', (req, res) => {
 
 })
 
+app.get("/api-test", (req,res)=>{
+
+    db.query(
+        "SELECT DATABASE() AS db",
+        (err,result)=>{
+
+            if(err){
+                return res.json({
+                    error:err.message
+                })
+            }
+
+            res.json(result)
+        }
+    )
+
+})
+
 module.exports = app
